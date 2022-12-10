6dab4e7f4bb15bda8aec5737899d403af3dbbfe3
hyunjeong <cchloe0927@gmail.com>
2022-12-11 01:44:05 +0900

    style : finish comments Layout

---
+++ src/components/detail/comments/AddComments.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+//style
+import classes from "./AddComments.module.css";
+//components
+import Button from "../../elements/Button";
+
+const AddComments = () => {
+  return (
+    <div>
+      <h3>Team's Todo Comments</h3>
+      <hr />
+      <form className={classes.addbox}>
+        <div className={classes.input_box}>
+          <div>
+            <label htmlFor="title">작성자</label>
+            <input id="title" name="title" type="text" />
+          </div>
+          <div>
+            <label htmlFor="contents">코멘트</label>
+            <textarea id="contents" name="contents" type="text" />
+          </div>
+        </div>
+        <Button>추가하기</Button>
+      </form>
+    </div>
+  );
+};
+
+export default AddComments;
--- src/components/detail/comments/Comments.jsx
+++ src/components/detail/comments/Comments.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+//style
+import classes from "./Comments.module.css";
+//components
+import AddComments from "./AddComments";
+import Card from "../../elements/Card";
+import Button from "../../elements/Button";
 
-const Comments = () => {
-  return <div>Comments</div>;
+const CommentsList = () => {
+  return (
+    <div>
+      <AddComments />
+      <Card className={classes.comment_wrap}>
+        <form className={classes.comment_box}>
+          <div className={classes.comment_header}>
+            <label>👤 이현정 님의 코멘트</label>
+            <div className={classes.btn}>
+              <Button>수정하기</Button>
+              <Button>삭제하기</Button>
+            </div>
+          </div>
+          <textarea
+            name="comment"
+            type="text"
+            className={classes.comment}
+            disabled
+          />
+        </form>
+      </Card>
+    </div>
+  );
 };
 
-export default Comments;
+export default CommentsList;
