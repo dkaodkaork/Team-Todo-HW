fae14d858cc009e6252fafa4ff19ff244980a2cb
dkaodkaork <dkaodkaork@gmail.com>
2022-12-12 16:02:21 +0900

    test

--- src/components/main/todolistcontainer/TodoListContainer.jsx
+++ src/components/main/todolistcontainer/TodoListContainer.jsx
@@ -7,10 +7,6 @@ const TodoListContainer = () => {
   const classname = "addbtn";
   const navigate = useNavigate();
 
-  // 좀더 UI를 컴포넌트 별로 나누게  해보셈
-  // 여기서 TodoList를 가져올 필요가 없을 것 같다
-  // ex 버튼 상단부를 나누고, 나머지 본문부를 나눌거 같음
-  //버ㅏ튼 상당부에서 버튼 onClick이런거 전 정의해줌
   return (
     <>
       <div className={classes.btnbox}>
